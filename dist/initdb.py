7c46ceb7e111024b1a37bebfc6e19c8c66136e8d
gilfoyle97 <35783570+edoardottt@users.noreply.github.com>
2019-06-19 10:09:08 +0200

    Add files via upload

--- dist/initdb.py
+++ dist/initdb.py
@@ -8,8 +8,8 @@ Created on Thu Apr 18 11:37:00 2019
 import sqlite3
 
 conn = sqlite3.connect('result.db')
-    
 c = conn.cursor()
+c.execute('drop table if exists match')
 c.execute("""CREATE TABLE match (
             date date,
             missili integer,
