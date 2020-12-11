690877177f360f2c8020f06155231ad0462c92c1
edoardottt <edoardott@gmail.com>
2020-12-11 14:02:39 +0100

    flake

--- initdb.py
+++ initdb.py
@@ -7,16 +7,18 @@ Created on Thu Apr 18 11:37:00 2019
 
 import sqlite3
 
-conn = sqlite3.connect('result.db')
-    
+conn = sqlite3.connect("result.db")
+
 c = conn.cursor()
-c.execute('drop table if exists match')
-c.execute("""CREATE TABLE match (
+c.execute("drop table if exists match")
+c.execute(
+    """CREATE TABLE match (
             date date,
             missili integer,
             bombe integer,
             nemici integer,
             PRIMARY KEY(date)
-            )""")
+            )"""
+)
 conn.commit()
 conn.close()
--- results.py
+++ results.py
@@ -7,37 +7,37 @@ Created on Thu Apr 18 11:07:47 2019
 
 import sqlite3
 from datetime import datetime
-    
 
-def save(missili,bombe,nemici):
 
-    conn = sqlite3.connect('result.db')
+def save(missili, bombe, nemici):
+
+    conn = sqlite3.connect("result.db")
     timenow = datetime.now()
-    
+
     c = conn.cursor()
-    c.execute("INSERT INTO  match VALUES(?,?,?,?)",(timenow,missili,bombe,nemici))
+    c.execute("INSERT INTO  match VALUES(?,?,?,?)", (timenow, missili, bombe, nemici))
     conn.commit()
-    
-    c.execute("SELECT missili,bombe,nemici FROM match WHERE date=?",(str(timenow),))
-    
+
+    c.execute("SELECT missili,bombe,nemici FROM match WHERE date=?", (str(timenow),))
+
     c.execute("SELECT MAX(missili) FROM match")
     bold = str(c.fetchone())
     bold = bold.replace("(", "")
     bold = bold.replace(")", "")
-    b = bold.replace(",","")
-    
+    b = bold.replace(",", "")
+
     c.execute("SELECT MAX(bombe) FROM match")
     aold = str(c.fetchone())
     aold = aold.replace("(", "")
     aold = aold.replace(")", "")
-    a = aold.replace(",","")
-    
+    a = aold.replace(",", "")
+
     c.execute("SELECT MAX(nemici) FROM match")
     dold = str(c.fetchone())
     dold = dold.replace("(", "")
     dold = dold.replace(")", "")
-    d = dold.replace(",","")
+    d = dold.replace(",", "")
     conn.commit()
-    
+
     conn.close()
-    return [a,b,d]
+    return [a, b, d]
